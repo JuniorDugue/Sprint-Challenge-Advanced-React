a5b117d2bdc7da78be0667e0b768f20d318dd5ff
Junior Dugue <jrdugue@gmail.com>
2019-10-12 13:40:10 -0400

    answered questions, kept running into issues with client folder, so i created a client2 folder to continue working, added jest and continous monitoring over testing via watchAll configuration, have server on port5000 running successfully

---
+++ client2/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <h2>Sprint Project</h2>
+     
+    </div>
+  );
+}
+
+export default App;
