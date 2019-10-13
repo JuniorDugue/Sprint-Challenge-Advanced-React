17714794012a8d0f32ded8b1f292fed225426d8e
Junior Dugue <jrdugue@gmail.com>
2019-10-13 00:00:04 -0400

    completed mvp, included basic tests will have to add more, converted functional comp to class, reused custom hooks for darkmode/localstorage

--- client2/src/App.js
+++ client2/src/App.js
@@ -1,13 +1,36 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
+import axios from 'axios';
+import Display from './components/Display';
+class App extends Component {
+  constructor(props) {
+    super();
 
-function App() {
-  return (
-    <div className="App">
-        <h2>Sprint Project</h2>
-     
-    </div>
-  );
+    this.state = {
+      users: [],
+    }
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:5000/api/players')
+      .then((res) => {
+        this.setState({
+          users: res.data
+        })
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+
+  render(){
+    return (
+      <div>
+        <h2 data-testid="header">Sprint Project</h2>
+      <Display userData={this.state.users}/>
+      </div>
+    )
+  }
 }
 
 export default App;
--- client2/src/App.test.js
+++ client2/src/App.test.js
@@ -1,9 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+// it('renders without crashing', () => {
+//   const div = document.createElement('div');
+//   ReactDOM.render(<App />, div);
+//   ReactDOM.unmountComponentAtNode(div);
+// });
+
+test("App renders without crashing", () => {
+  render(<App />);
+});
+
+test("contains sprint project", () => {
+  const container = render(<App/>);
+  console.log(container);
+
+  container.getByText(/sprint/i);
+  container.getByTestId(/header/i);
 });
